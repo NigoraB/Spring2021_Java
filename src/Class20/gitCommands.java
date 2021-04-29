a4d920e28580ea6a131cee72891fb006a6f53617
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-04-29 19:58:56 -0400

    git commands

---
+++ src/Class20/gitCommands.java
@@ -0,0 +1,19 @@
+package Class20;
+
+public class gitCommands {
+    /**
+     * to view all branches (local and remote)
+     * git branch -a
+     *
+     * to view local branches
+     * git branch
+     *
+     * Commands to create a branch
+     * git branch branchName
+     * git checkout branchName
+     *
+     *
+     * 
+     *
+     */
+}
