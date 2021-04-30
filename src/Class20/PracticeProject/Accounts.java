1a8f26ad7fdb6b4ddb8da7b0f286225aecef9d59
Deepak Sharma <sharma.deepak8616@gmail.com>
2021-04-29 21:24:25 -0400

    small project

---
+++ src/Class20/PracticeProject/Accounts.java
@@ -0,0 +1,4 @@
+package Class20.PracticeProject;
+
+public class Accounts {
+}
---
+++ src/Class20/PracticeProject/Customer.java
@@ -0,0 +1,40 @@
+package Class20.PracticeProject;
+
+public class Customer {
+    /**
+     * Customer ->
+     *      Name
+     *      SSN
+     *      Address
+     *
+     *      methods
+     *          editDetails()
+     *              input parameter : none
+     *              output : show the updated info.
+     *              make it interactive, ask user for the field wanna edit, and new value
+     *          createAccount()
+     *              input parameter : none
+     *              if input is savings, create a savings account
+     *              if input is checking, create a checking account
+     *              if input is something else, show error
+     *              make it interactive, ask user for account type
+     *          showAccountBalance()
+     *              input parameter : none
+     *              make it interactive, ask user for account number
+     *              show:
+     *                  Account number:
+     *                  Balance:
+     *          showAllAccountBalance()
+     *              show:
+     *                  You have total X account.
+     *                  Account number:
+     *                  Balance:
+     *
+     *                  Account number:
+     *                  Balance:
+     *
+     *                  Account number:
+     *                  Balance:
+     *
+     */
+}
